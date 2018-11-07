af9e17634aad24e21301e00b6eb076a2a184ddab
Mathias Mustonen <Mathias.Mustonen@gmail.com>
2018-11-07 09:50:09 +0200

    Basic folders and scripts

---
+++ Skee-Ball/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioMixer audioMixer;
+    private AudioClip[] musics;
+    private AudioClip[] sfxs;
+
+    private void Awake()
+    {
+        audioMixer = Resources.Load<AudioMixer>("Audio/Mixers/AudioMixer");
+        musics = Resources.LoadAll<AudioClip>("Audio/Music/");
+        sfxs = Resources.LoadAll<AudioClip>("Audio/Sfx/");
+    }
+}

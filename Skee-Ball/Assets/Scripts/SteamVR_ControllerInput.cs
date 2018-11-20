653ac2e4cf8f3e0e440ea185ae499274919c3b8a
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-11-20 10:28:34 +0200

    SteamVR controller grab

--- Skee-Ball/Assets/Scripts/SteamVR_ControllerInput.cs
+++ Skee-Ball/Assets/Scripts/SteamVR_ControllerInput.cs
@@ -17,14 +17,33 @@ public class SteamVR_ControllerInput : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider triggerCol) {
+
         if(joint == null && grabObj.GetStateDown(followedObj.inputSource)) {
+
             triggerCol = interactableCol;
             interactableObj.transform.position = controlAttachPoint.transform.position;
             joint = interactableObj.AddComponent<FixedJoint>();
             joint.connectedBody = controlAttachPoint;
+
             }
+
         else if(joint != null && grabObj.GetStateUp(followedObj.inputSource)){
 
+
+            var tempRigidbody = joint.GetComponent<Rigidbody>();
+            Object.DestroyImmediate(joint);
+            joint = null;
+
+            var origin = followedObj.origin ? followedObj.origin : followedObj.transform.parent;
+            if(origin != null) {
+                tempRigidbody.velocity = origin.TransformVector(followedObj.GetVelocity());
+                tempRigidbody.angularVelocity = origin.TransformVector(followedObj.GetAngularVelocity());
+
+            } else {
+                tempRigidbody.velocity = followedObj.GetVelocity();
+                tempRigidbody.angularVelocity = followedObj.GetAngularVelocity();
+            }
+            tempRigidbody.maxAngularVelocity = tempRigidbody.angularVelocity.magnitude;
         }
     }
 }

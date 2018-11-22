938092f031749a5b637b2c389e37f11e7de722f9
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-11-22 18:02:05 +0200

    SteamVR_ControllerInput koodin muokkausta.

--- Skee-Ball/Assets/Scripts/SteamVR_ControllerInput.cs
+++ Skee-Ball/Assets/Scripts/SteamVR_ControllerInput.cs
@@ -6,44 +6,62 @@ public class SteamVR_ControllerInput : MonoBehaviour
     [SteamVR_DefaultAction("Interact")]
     public SteamVR_Action_Boolean grabObj;
 
+    // OntriggerEnter ja -Exit muuttuja
+    public Rigidbody interactableRig;
+
+    // Vuorovaikutettavan esineen muuttuja
     public GameObject interactableObj;
-    public Collider interactableCol;
-    public Rigidbody controlAttachPoint;
-    SteamVR_Behaviour_Pose followedObj;
+
+    // Vuorovaikutettavaan esineen sijaintiin vaikuttava muuttuja ohjaimessa
+    public Rigidbody controllerAttachPoint;
+
+    // Steamin oma Pose funktio
+    SteamVR_Behaviour_Pose controllerInput;
+
     FixedJoint joint;
+
+    /*
+
 
     void Awake() {
         followedObj = GetComponent<SteamVR_Behaviour_Pose>();
     }
 
     void OnTriggerEnter(Collider triggerCol) {
+        if (!triggerCol.GetComponent<Rigidbody>()) {
+            return;
+        }
+        collidingObj = triggerCol.gameObject;        
+    }
+    void OnTriggerExit(Collider triggerCol) {
+        collidingObj = null;
+    }
 
+    void FixedUpdate() {
         if(joint == null && grabObj.GetStateDown(followedObj.inputSource)) {
-
-            triggerCol = interactableCol;
+            // Tähän if(collidingObj){
             interactableObj.transform.position = controlAttachPoint.transform.position;
             joint = interactableObj.AddComponent<FixedJoint>();
             joint.connectedBody = controlAttachPoint;
-
-            }
-
-        else if(joint != null && grabObj.GetStateUp(followedObj.inputSource)){
-
-
+        }
+        else if(joint != null && grabObj.GetStateUp(followedObj.inputSource)) {
+            // Tähän if(ObjectinHand){
             var tempRigidbody = joint.GetComponent<Rigidbody>();
             Object.DestroyImmediate(joint);
             joint = null;
 
             var origin = followedObj.origin ? followedObj.origin : followedObj.transform.parent;
+
             if(origin != null) {
                 tempRigidbody.velocity = origin.TransformVector(followedObj.GetVelocity());
                 tempRigidbody.angularVelocity = origin.TransformVector(followedObj.GetAngularVelocity());
-
-            } else {
+            }
+            else {
                 tempRigidbody.velocity = followedObj.GetVelocity();
                 tempRigidbody.angularVelocity = followedObj.GetAngularVelocity();
             }
             tempRigidbody.maxAngularVelocity = tempRigidbody.angularVelocity.magnitude;
         }
     }
+    */
 }

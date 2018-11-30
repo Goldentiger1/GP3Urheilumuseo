74b8a94050a10ebe6aaaa84b0404213427e4cc7e
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-11-30 14:03:43 +0200

    Kori ominaisuutta.

---
+++ Skee-Ball/Assets/Scripts/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+    public GameObject hoop;
+    public GameObject basketball;
+
+    void Start() {
+        hoop = GameObject.Find("Basketball hoop");
+        
+    }
+}

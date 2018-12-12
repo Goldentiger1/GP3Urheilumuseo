89b8d01e0b89fad89f00d37bd1db3cb6d0d8e070
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-12-12 13:55:57 +0200

    Reseti buttonia

---
+++ Skee-Ball/Assets/Scripts/BallTransformReset.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransformReset : MonoBehaviour {
+    public Collider trigger;
+    public GameObject controller;
+    public float time;
+    public List<GameObject> basketballs = new List<GameObject>();
+
+    void Start() {
+        trigger = this.gameObject.GetComponent<Collider>();
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(other.gameObject != null) {
+            return;
+        }
+        controller = other.gameObject;
+    }
+
+    void Update() {
+        if(controller != null) {
+            time += Time.deltaTime;
+        }if(time == 5) {
+
+        }
+    }
+}

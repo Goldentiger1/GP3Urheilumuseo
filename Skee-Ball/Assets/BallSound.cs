e9334316f259280dbf16aeaec9c697b32370dc8e
Mathias Mustonen <Mathias.Mustonen@gmail.com>
2018-12-12 14:46:03 +0200

    Some sounds.

---
+++ Skee-Ball/Assets/BallSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSound : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            print("Pallo pomppii");
+            Fabric.EventManager.Instance.PostEvent("ballbounce");
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Fabric.EventManager.Instance.PostEvent("street");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

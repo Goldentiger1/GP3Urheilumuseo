f2dd72e5e69a81fe576e72934d6115cbe94ebf4c
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2018-12-17 13:07:06 +0200

    Scenen vaihtoa.

---
+++ Skee-Ball/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public float timer;
+    public float setVisibleTime;
+    public GameObject button;
+
+    private void Start() {
+        button = gameObject.GetComponentInChildren<GameObject>();
+    }
+
+    void Update() {
+        timer = Time.deltaTime;
+        button.SetActive(false);
+    }
+}

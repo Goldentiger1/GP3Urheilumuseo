176d77bd4cf1b13ee7e94e00de58d0e57fd98dcc
Mathias Mustonen <Mathias.Mustonen@gmail.com>
2019-01-17 14:36:56 +0200

    Steam fix?

--- Skee-Ball/Assets/Scripts/Managers/LocalizationManager.cs
+++ Skee-Ball/Assets/Scripts/Managers/LocalizationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [SerializeField]
@@ -14,7 +15,41 @@ public class LocalizationItem
     public string Value;
 }
 
-public class LocalizationManager : MonoBehaviour
+public class LocalizationManager : Singelton<LocalizationManager>
 {
-   
+    private Dictionary<string, string> localizationText;
+
+    private void Start()
+    {
+        //LoadLocalizedText("LocalizedText_UK.json");
+    }
+
+    public void LoadLocalizedText(string fileName)
+    {
+        localizationText = new Dictionary<string, string>();
+
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName );
+        Debug.Log(filePath);
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+
+            print("Json: " + dataAsJson);
+
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+            print("Loaded Items: " + loadedData.Items.Length);
+
+            for (int i = 0; i < loadedData.Items.Length; i++)
+            {
+                localizationText.Add(loadedData.Items[i].Key, loadedData.Items[i].Value);
+            }
+
+            Debug.Log("Data loaded, dictionary contains: " + localizationText.Count + " entries.");
+        }
+        else
+        {
+            Debug.LogError("Cannot find file!");
+        }
+    }
 }
